0a212733927ba85959537faef59902acf3141563
arefehkompani <awriik00@gmail.com>
2022-04-30 18:01:26 +0430

    Start the journey

--- src/ros_pro2/src/controller_step3.py
+++ src/ros_pro2/src/controller_step3.py
@@ -88,8 +88,8 @@ class Controller:
             X = np.concatenate([X1, X2, X3, X4])
             Y = np.concatenate([Y1, Y2, Y3, Y4])
 
-            self.x_inputs = np.array(X)
-            self.y_inputs = np.array(Y)
+            self.x_inputs = X
+            self.y_inputs = Y
 
         elif number == 3:
             growth_factor = 0.1
@@ -105,9 +105,52 @@ class Controller:
             self.y_inputs = np.array(Y)
 
         else: 
+            X1 = np.linspace(-1, 1 , 100)
+            Y1 = np.array([3]*100)
+
+            X2 = np.linspace(1, 1 + 2**(1/2) , 100)
+            Y2 = - (2**(1/2)) * (X2 - 1) + 3
+
+            Y3 = np.linspace(1, -1 , 100)
+            X3 = np.array([1 + 2**(1/2)]*100)
+
+            X4 = np.linspace(1 + 2**(1/2), 1, 100)
+            Y4 = (2**(1/2)) * (X4 - 1 - 2**(1/2)) -1 
+
+            X5 = np.linspace(1, -1 , 100)
+            Y5 = np.array([-3]*100)
+
+            X6 = np.linspace(-1, -1 - 2**(1/2) , 100)
+            Y6 = - (2**(1/2)) * (X6 + 1) - 3 
 
 
+            Y7 = np.linspace(-1, 1 , 100)
+            X7 = np.array([- 1 - 2**(1/2)]*100)
 
+
+            X8 = np.linspace(-1 - 2**(1/2), -1, 100)
+            Y8 = (2**(1/2)) * (X8 + 1 + 2**(1/2)) + 1
+
+            X = np.concatenate([X1,X2,X3,X4,X5,X6,X7,X8])
+            Y = np.concatenate([Y1,Y2,Y3,Y4,Y5,Y6,Y7,Y8])
+
+            self.x_inputs = X
+            self.y_inputs = Y
+
+    def start(self):
+        goal = Point()
+        msg = rospy.wait_for_message("/odom", Odometry)
+        position = msg.pose.position
+        for i in range(self.x_inputs.size):
+            error = [sqrt(pow(self.x_inputs[i]-position.x, 2) + pow(self.y_inputs[i] - position.y, 2))]
+        
+        nearest_point = min(enumerate(error), key=lambda x: x[1])[0]
+
+        goal.x = self.x_inputs[nearest_point]
+        goal.y = self.y_inputs[nearest_point]
+
+        self.g_index = nearest_point
+        self.goal = goal
 if __name__ == "__main__":
     controller = Controller()
     rospy.spin()
